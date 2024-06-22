48f044ddb96d6fce5a630eaad0dbb7ee85ac30ec
Sahil Saurabh <60626650+SahilSaurabh734@users.noreply.github.com>
2024-06-22 22:30:31 +0530

    Update Strings.sv

--- Strings.sv
+++ Strings.sv
@@ -3,8 +3,8 @@
 `timescale 1ns/1ps
 module tb;
   
-  string s1="Hello World";
-  string s2="sahil";
+  string s1="sahil";
+  string s2="saurabh";
   string s3="NITD";
   string s4="nitd";
   
@@ -13,12 +13,12 @@ module tb;
     $display("Sting 2: %s",s2);
     $display("Length of String 1 : %0d",s1.len());
     $display("Length of String 2 : %0d",s2.len());
-    s1.putc(3,"2");
+    s1.putc(3,"O");
     $display("String 1 : %s",s1);
     $display("Compare:%0d",s2.compare(s1));
     $display("I_Compare:%0d",s1.icompare(s2));
     $display("I_Compare2:%0d",s3.icompare(s4));
-    $display("Compare2:%0d",s3.compare(s4));
+    $display("Compare2:%0d",s1.compare(s2));
     $display("S1: %s",s1.tolower());
    end
   
@@ -28,10 +28,16 @@ module tb;
     else
       $display("Fail");
   
-  if(s3 == s4)
+    if(s3 == s4)
       $display("Pass");
     else
       $display("Fail");
+    
+    if(s1>s2)
+      $display("S1>S2");
+    else if (s1<s2)
+      $display("S1<S2");
+    else $display("S1==S2");
   end
 endmodule
     
