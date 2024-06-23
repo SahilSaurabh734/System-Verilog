8a036683a8339d611dad63d79324d628a310ce5e
Sahil Saurabh <60626650+SahilSaurabh734@users.noreply.github.com>
2024-06-23 09:39:36 +0530

    Update struct_with_function.sv

--- struct_with_function.sv
+++ struct_with_function.sv
@@ -46,6 +46,13 @@ module tb();
     $display("Branch:%s",s.branch.name());
     
   endfunction
+  
+  function reg compare(student s1, student s2);
+    if((s1.name==s2.name) && (s1.roll==s2.roll)&&(s1.add.house_num==s1.add.house_num)&&(s1.add.street==s2.add.street)&&(s1.add.state==s2.add.state)&&(s1.add.pincode==s2.add.pincode)&&(s1.year==s2.year)&&(s1.branch==s2.branch))
+      $display("Student 1 and Student 2 is same");
+    else
+      $display("Student 1 and Student 2 is different");
+    endfunction
   initial begin
     stud[0] = fillDetail(stud[0],"sahil",222221018,5,"nav","bihar",800025,2024,ECE);
     stud[1] = fillDetail(stud[1],"saurabh",222221017,3,"raja_bazar","bihar",800021,2024,CSE);
@@ -59,6 +66,8 @@ module tb();
       printdetail(stud[i]);
     $display("-----------------------------------");
     end
+    
+    compare(stud[0],stud[0]);
   end
 endmodule
   
